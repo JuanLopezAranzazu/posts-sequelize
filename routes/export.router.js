3c3adedac18f126ec0860961f9b725602dfbaa80
Juan Esteban Lopez Aranzazu <juanlopezaranzazu@gmail.com>
2022-12-27 18:04:59 -0500

    update exports columns

--- routes/export.router.js
+++ routes/export.router.js
@@ -14,16 +14,36 @@ const {
   filterUserSchema,
 } = require("./../schemas/post.schema");
 
-function createWorkSheet(callback) {
+const HANDLE_DATA_KEYS = {
+  id: "Id",
+  title: "Title",
+  description: "Description",
+  createdAt: "Date",
+};
+
+function createArrayColumns(columns) {
+  return columns.map((item) => {
+    const dataForColumn = {
+      header: HANDLE_DATA_KEYS[item],
+      key: item,
+      width: 10,
+    };
+    return dataForColumn;
+  });
+}
+
+function createWorkSheet(columns, callback) {
   const workbook = new excelJS.Workbook();
   const worksheet = workbook.addWorksheet("data");
 
-  worksheet.columns = [
-    { header: "Id", key: "id", width: 10 },
-    { header: "Title", key: "title", width: 10 },
-    { header: "Description", key: "description", width: 10 },
-    { header: "Created at", key: "createdAt", width: 10 },
-  ];
+  worksheet.columns = columns
+    ? columns
+    : [
+        { header: "Id", key: "id", width: 10 },
+        { header: "Title", key: "title", width: 10 },
+        { header: "Description", key: "description", width: 10 },
+        { header: "Date", key: "createdAt", width: 10 },
+      ];
 
   const filtered = callback;
   filtered.forEach((item) => {
@@ -46,13 +66,17 @@ exportRouter.get(
   async (req, res, next) => {
     try {
       const { body } = req;
-      const { entryDates } = body;
+      const { entryDates, columns } = body;
 
       const path = "./files";
       const fileName = "filter-date.xlsx";
 
       const posts = await postService.findAll();
-      const workbook = createWorkSheet(getPostsByDates(posts, entryDates));
+      const arrayColumns = !columns ? undefined : createArrayColumns(columns);
+      const workbook = createWorkSheet(
+        arrayColumns,
+        getPostsByDates(posts, entryDates)
+      );
 
       await workbook.xlsx.writeFile(`${path}/${fileName}`).then(() => {
         res.send({
@@ -76,13 +100,17 @@ exportRouter.get(
   async (req, res, next) => {
     try {
       const { body } = req;
-      const { entryArray } = body;
+      const { entryArray, columns } = body;
 
       const path = "./files";
       const fileName = "filter-user.xlsx";
-
       const posts = await postService.findAll();
-      const workbook = createWorkSheet(getPostsByUser(posts, entryArray));
+
+      const arrayColumns = !columns ? undefined : createArrayColumns(columns);
+      const workbook = createWorkSheet(
+        arrayColumns,
+        getPostsByUser(posts, entryArray)
+      );
 
       await workbook.xlsx.writeFile(`${path}/${fileName}`).then(() => {
         res.send({
--- schemas/post.schema.js
+++ schemas/post.schema.js
@@ -3,24 +3,23 @@ const Joi = require("joi");
 const id = Joi.number().integer();
 const title = Joi.string();
 const description = Joi.string();
-const userId = Joi.number().integer();
 // filter posts
 const item = Joi.number().integer();
+const key = Joi.string();
 const entryDates = Joi.array().items(
   Joi.object({ year: item, month: item, date: item })
 );
 const entryArray = Joi.array().items(item);
+const columns = Joi.array().items(key);
 
 const createPostSchema = Joi.object({
   title: title.required(),
   description: description.required(),
-  userId: userId.required(),
 });
 
 const updatePostSchema = Joi.object({
   title: title,
   description: description,
-  userId: userId,
 });
 
 const getPostSchema = Joi.object({
@@ -30,10 +29,12 @@ const getPostSchema = Joi.object({
 // filter posts
 const filterDateSchema = Joi.object({
   entryDates: entryDates.required(),
+  columns: columns,
 });
 
 const filterUserSchema = Joi.object({
   entryArray: entryArray.required(),
+  columns: columns,
 });
 
 module.exports = {
